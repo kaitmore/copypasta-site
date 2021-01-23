0b88e2de09b30572652645a8e1781220b1d22b7d
kaitmore <kaitmore@gmail.com>
2021-01-23 11:15:47 -0500

    try download funcion

---
+++ src/functions/download/download.js
@@ -0,0 +1,39 @@
+const { Octokit } = require("@octokit/rest");
+
+/* eslint-disable */
+const options = {
+  owner: process.env.GH_OWNER || "kaitmore",
+  repo: process.env.GH_REPO || "copypasta",
+  token: process.env.GH_TOKEN
+};
+
+exports.handler = async function (event, context) {
+  const { owner, repo, token } = options;
+  const github = new Octokit({ auth: token });
+  const { data: releases } = await github.repos.listReleases({
+    owner,
+    repo
+  });
+  const latestRelease = releases.shift();
+
+  return {
+    statusCode: 200,
+    body: `Download ${latestRelease.browser_download_url}`
+  };
+
+  // let asset_id;
+  // try {
+  //   asset_id = latestRelease.assets.find((a) => a.name.endsWith(".dmg")).id;
+  // } catch (e) {
+  //   console.error("Could not find the latest release asset");
+  //   process.exit(1);
+  // }
+  // console.log(latestRelease.assets);
+  // const asset = await github.repos.getReleaseAsset({
+  //   owner,
+  //   repo,
+  //   asset_id
+  // });
+
+  // console.log("asset", asset);
+};
