1ff617c035b13d121cfc99a434d7f807cbeded6b
kaitmore <kaitmore@gmail.com>
2021-01-30 14:36:15 -0500

    move to db

--- src/functions/stripe/db.js
+++ src/functions/stripe/db.js
@@ -1,6 +1,16 @@
 var faunadb = require("faunadb"),
   q = faunadb.query;
 
-const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
+require("dotenv").config();
 
+console.log("process.env.FAUNADB_SECRET", process.env.FAUNADB_SECRET);
+const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
+client
+  .query(
+    q.Create(q.Collection("licenses"), {
+      data: { title: "What I had for breakfast .." }
+    })
+  )
+  .then((ret) => console.log("RETURN QUERY", ret))
+  .catch(console.log);
 module.exports = { client, q };
--- src/functions/stripe/stripe.js
+++ src/functions/stripe/stripe.js
@@ -8,14 +8,6 @@ exports.handler = async function (req, context) {
   if (event.type === "payment_intent.succeeded") {
     const paymentIntent = event.data.object;
     console.log(paymentIntent);
-    client
-      .query(
-        q.Create(q.Collection("licenses"), {
-          data: { title: "What I had for breakfast .." }
-        })
-      )
-      .then((ret) => console.log("RETURN QUERY", ret))
-      .catch(console.log);
     // Then define and call a method to handle the successful payment intent.
   } else {
     console.log(`Unhandled event type ${event.type}`);
