b4d1a5a205720813691d3fccdd5e2d7052fd590f
kaitmore <kaitmore@gmail.com>
2021-01-30 15:17:07 -0500

    add bcrypt

--- src/functions/stripe/stripe.js
+++ src/functions/stripe/stripe.js
@@ -1,22 +1,28 @@
 let { client, q } = require("./db.js");
+const bcrypt = require("bcrypt");
+const saltRounds =  process.env.SALT_ROUNDS || 10;
 
 // Match the raw body to content type application/json
 exports.handler = async function (req, context) {
   const event = JSON.parse(req.body);
-
+  let resp;
   // Handle the event
   if (event.type === "payment_intent.succeeded") {
     const paymentIntent = event.data.object;
-    console.log(paymentIntent);
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(paymentIntent.id, salt, function (err, hash) {
+        // Store hash in database here
+        resp = await client.query(
+          q.Create(q.Collection("licenses"), {
+            data: { [`${Math.random()}@gmail.com`]: hash }
+          })
+        );
+      });
+    });
     // Then define and call a method to handle the successful payment intent.
   } else {
     console.log(`Unhandled event type ${event.type}`);
   }
-  const ret = await client.query(
-    q.Create(q.Collection("licenses"), {
-      data: { title: "What I had for breakfast .." }
-    })
-  );
 
   return {
     statusCode: 200,
