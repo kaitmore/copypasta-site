589439b481bd29811e8ab180e5045da2d33ae614
kaitmore <kaitmore@gmail.com>
2021-01-30 14:31:20 -0500

    revert

--- src/functions/stripe/stripe.js
+++ src/functions/stripe/stripe.js
@@ -8,12 +8,12 @@ exports.handler = async function (req, context) {
   if (event.type === "payment_intent.succeeded") {
     const paymentIntent = event.data.object;
     console.log(paymentIntent);
-    // client
-    //   .query(
-    q.Create(q.Collection("licenses"), {
-      data: { title: "What I had for breakfast .." }
-    })
-      //   )
+    client
+      .query(
+        q.Create(q.Collection("licenses"), {
+          data: { title: "What I had for breakfast .." }
+        })
+      )
       .then((ret) => console.log("RETURN QUERY", ret))
       .catch(console.log);
     // Then define and call a method to handle the successful payment intent.
