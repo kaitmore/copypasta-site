ab1a666875a0cc4dabdd9d770c91301bfd05a711
kaitmore <kaitmore@gmail.com>
2021-01-31 12:35:37 -0500

    statusCode

--- src/functions/validate-license/index.js
+++ src/functions/validate-license/index.js
@@ -8,19 +8,19 @@ exports.handler = async function ({ queryStringParameters }, context) {
     const isValid = await isLicenseValid(licenseKey);
     if (isValid) {
       return {
-        status: 200,
+        statusCode: 200,
         body: "License is valid"
       };
     } else {
       return {
-        status: 404,
+        statusCode: 404,
         body: "License is not valid"
       };
     }
     // TODO: lookup hashedLicense in db, if found, license key is valid, else invalid
   } catch (e) {
     return {
-      status: 400,
+      statusCode: 400,
       body: JSON.stringify(e)
     };
   }
