ea675097caab38d6271a583a06ed8eafbb669044
kaitmore <kaitmore@gmail.com>
2021-01-31 12:26:18 -0500

    fix isLicenseValid func

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,5 +1,7 @@
 var faunadb = require("faunadb"),
   q = faunadb.query;
+const { compare } = require("./hash");
+require("dotenv").config();
 
 const DOCUMENT_NAME = process.env.DOCUMENT_NAME || "licenses";
 const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
@@ -12,19 +14,40 @@ const setLicenseKey = async (email, name, licenseKey) =>
   );
 
 const isLicenseValid = async (licenseKey) => {
-  let resp;
+  let result;
   try {
-    resp = await client.query(
-      q.Map(
-        q.Paginate(q.Documents(q.Collection("licenses"))),
-        q.Lambda((x) => q.Get(x))
-      )
-    );
+    result = _getPaginatedEntries();
+    _findMatchingLicense(result, licenseKey);
+    if (result.after) {
+      while (result.after) {
+        result = _getPaginatedEntries(result.after);
+        _findMatchingLicense(result, licenseKey);
+      }
+    }
   } catch (e) {
-    console.log("ERROR IN isLicenseValid", e);
+    console.log("ERROR in isLicenseValid", e);
   }
-  console.log("RESP", resp);
-  return true;
+  console.log("No matches");
+
+  return false;
 };
 
+const _getPaginatedEntries = async (after) => {
+  let opts = {};
+  if (after) opts.after = after;
+  return await client.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection("licenses")), opts),
+      q.Lambda((x) => q.Get(x))
+    )
+  );
+};
+
+const _findMatchingLicense = async (entries, licenseKey) => {
+  for (let i = 0; i < entries.length; i++) {
+    const license = entries[i].data;
+    const isValid = await compare(license.licenseKey, licenseKey);
+    if (isValid) return true;
+  }
+};
 module.exports = { setLicenseKey, isLicenseValid };
