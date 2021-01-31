212b5c83e045922cf4e42c3e1d41ccf1237820f7
kaitmore <kaitmore@gmail.com>
2021-01-31 10:10:40 -0500

    return e

--- src/functions/validate-license/validate.js
+++ src/functions/validate-license/validate.js
@@ -17,7 +17,8 @@ exports.handler = async function ({ queryStringParameters }, context) {
     // TODO: lookup hashedLicense in db, if found, license key is valid, else invalid
   } catch (e) {
     return {
-      status: 400
+      status: 400,
+      body: JSON.stringify(e)
     };
   }
 };
