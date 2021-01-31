18e0f39233a3b9056892246180414a4d875738c9
kaitmore <kaitmore@gmail.com>
2021-01-31 10:02:27 -0500

    add return

--- src/functions/validate-license/index.js
+++ src/functions/validate-license/index.js
@@ -10,6 +10,10 @@ exports.handler = async function ({ queryStringParameters }, context) {
     const isLicenseValid = await lookupLicenseKey(hashedLicenseKey);
 
     console.log("isLicenseValid", isLicenseValid);
+    return {
+      status: 200,
+      body: JSON.stringify(isLicenseValid)
+    };
     // TODO: lookup hashedLicense in db, if found, license key is valid, else invalid
   } catch (e) {
     return {
