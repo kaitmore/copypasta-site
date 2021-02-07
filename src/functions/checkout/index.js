8bfc841a2798bca3c6428e17a2a4d9552a5b6901
kaitmore <kaitmore@gmail.com>
2021-02-07 10:27:19 -0500

    add checkout

---
+++ src/functions/checkout/index.js
@@ -0,0 +1,27 @@
+// Set your secret key. Remember to switch to your live secret key in production!
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+// handles a license key from a user and returns whether or not it is valid
+exports.handler = async function (req, context) {
+  let session;
+  try {
+    session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      lineItems: [{ price: "price_1IIDzYEZYj5kKn7UluwG4CiT", quantity: 1 }],
+      mode: "payment",
+      success_url: "https://copypasta.sh/success",
+      cancel_url: "https://copypasta.sh/cancel"
+    });
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e)
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ id: session.id })
+  };
+};
--- src/site/_includes/js/core.js
+++ src/site/_includes/js/core.js
@@ -1,10 +1,30 @@
-// simple button click event handler
-function btnHandler(selector, callback) {
-  var btn = document.querySelector(selector);
-  if(!btn) { return; }
-  btn.addEventListener('click', function(event) {
-    event.preventDefault();
-    callback();
-  }, false);
-}
+var stripe = Stripe(
+  "pk_live_51IFLVtEZYj5kKn7UwCTDiL2O9YyI1QrrxT9XUrZhBUbPBradRMFQKhckjA3CgrkyieRreU8MxB5sDVfc8tfqPUMR00txcBkYex"
+);
+var checkoutButton = document.getElementById("checkout-button");
 
+checkoutButton.addEventListener("click", function () {
+  // Create a new Checkout Session using the server-side endpoint you
+  // created in step 3.
+  fetch("/checkout", {
+    method: "POST"
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (session) {
+      console.log(session);
+      return stripe.redirectToCheckout({ sessionId: session.id });
+    })
+    .then(function (result) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, you should display the localized error message to your
+      // customer using `error.message`.
+      if (result.error) {
+        alert(result.error.message);
+      }
+    })
+    .catch(function (error) {
+      console.error("Error:", error);
+    });
+});
